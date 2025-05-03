bb0dbbc953836fad21e2c46a60568f1df0856505
bimal009 <pandeybimal616@gmail.com>
2025-05-03 20:24:09 +0545

    Update Next.js configuration for static export and add deployment scripts. Include new dependencies for gh-pages and adjust build process.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
  eslint:{
   ignoreDuringBuilds:true
- }
+ },
+ basePath: '/Bimals_terminal',
 };
 
 export default nextConfig;
